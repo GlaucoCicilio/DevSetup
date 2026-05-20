44ac5ec6ec20c2d33750ff577100006d2140ce81
GlaucoCicilio <euanimal@hotmail.com>
2026-05-20 05:05:37 -0300

    Add check for existing Visual Studio Build Tools installation

--- DevSetup/installers/visualstudio.ps1
+++ DevSetup/installers/visualstudio.ps1
@@ -5,10 +5,14 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
 function Ensure-VisualStudioBuildTools {
-
+    if (Is-Installed "visualstudio") {
+        Log "Visual Studio Build Tools já instalado"
+        return
+    }
+    
     Invoke-WinGetInstall `
         -Id "Microsoft.VisualStudio.2022.BuildTools" `
         -ExtraArgs '--override "--wait --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended"'
-
+    
     Mark-Installed "visualstudio"
 }
