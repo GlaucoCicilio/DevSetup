cc140d0615457ed85045287fc43d6e742bf5e301
GlaucoCicilio <euanimal@hotmail.com>
2026-05-20 05:08:42 -0300

    Enhance MSYS2 installation script
    Updated the date and improved the MSYS2 installation check and logging.

--- DevSetup/installers/msys2.ps1
+++ DevSetup/installers/msys2.ps1
@@ -1,32 +1,36 @@
 # msys2.ps1
-# 2026-05-19
+# 2026-05-20
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
 function Ensure-MSYS2 {
-
-    if (Test-Path "C:\msys64") {
-        Log "MSYS2 já instalado"
+    $bash = "C:\msys64\usr\bin\bash.exe"
+    
+    # Verificação melhorada
+    if ((Test-Path "C:\msys64") -and (Test-Path $bash)) {
+        Log "MSYS2 já instalado e configurado"
         return
     }
-    else {
+    
+    if (!(Test-Path "C:\msys64")) {
+        Log "Instalando MSYS2..."
         Invoke-WinGetInstall -Id "MSYS2.MSYS2"
+        Start-Sleep -Seconds 5  # Aguarda instalação completar
     }
-
-    $bash = "C:\msys64\usr\bin\bash.exe"
-
+    
+    # Atualizar e instalar ferramentas
+    Log "Configurando MSYS2..."
     Start-Process `
         -FilePath $bash `
         -ArgumentList "-lc `"pacman -Syu --noconfirm`"" `
         -Wait
-
+    
     Start-Process `
         -FilePath $bash `
         -ArgumentList "-lc `"pacman -S --noconfirm mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb mingw-w64-x86_64-make`"" `
         -Wait
-
+    
     Add-ToPath "C:\msys64\mingw64\bin"
-
     Mark-Installed "msys2"
 }
