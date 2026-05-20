3f832324bfafecfcc6d3db8b6ed77fea1e9f79e8
GlaucoCicilio <euanimal@hotmail.com>
2026-05-20 05:32:27 -0300

    Update library installation script with new libraries

--- DevSetup/installers/libraries.ps1
+++ DevSetup/installers/libraries.ps1
@@ -1,24 +1,36 @@
 # libraries.ps1
-# 2026-05-19
+# 2026-05-20
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
 function Install-DefaultLibraries {
-
     $libraries = @(
         "boost",
         "fmt",
         "spdlog",
         "nlohmann-json",
         "sqlite3",
-        "openssl"
+        "openssl",
+        "openxlsx"
     )
 
+    $failed = @()
+
     foreach ($lib in $libraries) {
-
         Log "Instalando biblioteca: $lib"
+        
+        try {
+            & "$Global:VcpkgRoot\vcpkg.exe" install "$lib:x64-windows"
+            Log "Biblioteca instalada: $lib"
+        }
+        catch {
+            Log-Error "Falha ao instalar: $lib"
+            $failed += $lib
+        }
+    }
 
-        & "$Global:VcpkgRoot\vcpkg.exe" install "$lib:x64-windows"
+    if ($failed.Count -gt 0) {
+        Log-Warn "Bibliotecas que falharam: $($failed -join ', ')"
     }
 }
